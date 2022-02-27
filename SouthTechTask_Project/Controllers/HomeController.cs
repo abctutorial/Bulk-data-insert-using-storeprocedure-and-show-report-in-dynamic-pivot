ae9f1ea18beea21946fec7802d1d80115e5f7c1e
Reza Karim <mrezak16@gmail.com>
2022-02-28 00:56:05 +0600

    3rd commit

---
+++ SouthTechTask_Project/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using SouthTechTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SouthTechTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult SaveData(List<EmployeeBonusDetail> listOfDataToSave)
+        {
+            List<string> savedRecord = new List<string>();
+            try
+            {
+                DataTable tvp = new DataTable();
+                tvp.Columns.Add(new DataColumn("Emp_Id", typeof(string)));
+                tvp.Columns.Add(new DataColumn("Emp_Name", typeof(string)));
+                tvp.Columns.Add(new DataColumn("Emp_Bonus", typeof(string)));
+
+                // populate DataTable from my List here
+                foreach (var item in listOfDataToSave)
+                    tvp.Rows.Add(new object[] { item.EmployeeId, item.EmployeeName, item.EmployeeBonusAmount });
+                SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=TechnicalTest;Integrated Security=True");
+
+                SqlCommand cmd = new SqlCommand("dbo.SaveEmpBonusRecord", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter tvparam = cmd.Parameters.AddWithValue("@arrayList", tvp);
+                tvparam.SqlDbType = SqlDbType.Structured;
+                tvparam.Direction = ParameterDirection.Input;
+                tvparam.TypeName = "dbo.tpEmpBonus_List";
+
+                conn.Open();
+                SqlDataReader myReader = cmd.ExecuteReader();
+                while (myReader.Read())
+                {
+                    savedRecord.Add(myReader.GetString(0));
+                }
+                myReader.Close();
+            }
+            catch (Exception ex)
+            {
+                savedRecord.Add(ex.Message);
+            }
+            return Json(savedRecord, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ SouthTechTask_Project/Models/EmployeeBonusDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SouthTechTask.Models
+{
+    public class EmployeeBonusDetail
+    {
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public decimal EmployeeBonusAmount { get; set; }
+    }
+}
